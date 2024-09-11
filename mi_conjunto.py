b718af6e26e837152485a11dce4ac3a049d9b2e9
Danielalopez-128 <a.22308051281240@cbtis128.edu.mx>
2024-09-11 07:48:07 -0600

    Add files via upload

---
+++ mi_conjunto.py
@@ -0,0 +1,6 @@
+print("Ejemplos de conjuntos set")
+tipodeportes={"Futbol","Tenis","Basquetbol"}
+print(tipodeportes)
+print("Lista elementos del conjunto tipodeportes loop form")
+for lopez in tipodeportes:
+    print(lopez)
---
+++ mi_diccionario.py
@@ -0,0 +1,8 @@
+diccionario = {
+  "Nombre": "Daniela",
+  "Apellidos": "López Chavira",
+  "Fecha de nacimiento": 2007
+}
+print(diccionario)
+for lopez in diccionario:
+  print(diccionario[lopez])
